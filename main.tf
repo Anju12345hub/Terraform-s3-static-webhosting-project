836763df2c95a3c867ea48e0c48ac94dc6cc7d7e
Anju12345hub <127766118+Anju12345hub@users.noreply.github.com>
2023-09-11 14:25:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ resource "aws_s3_object" "error" {
 
 resource "aws_s3_object" "profile" {
   bucket = aws_s3_bucket.bucketweb.id
-  key    = "profile.jpg"
-  source = "profile.jpg"
+  key    = "profile1.jpg"
+  source = "profile1.jpg"
   acl = "public-read"
   
 }
